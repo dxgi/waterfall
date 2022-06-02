69bda4e7151a8af9c94c07aefb88bd8a2b2c211a
iMaximus <61899298+iiiMaximus@users.noreply.github.com>
2022-06-02 16:20:00 +0800

    remove callback outside

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ const bind = async (fn: Function, cb: Function, args: any) => {
 export function waterfall(tasks: Array<Function>, callback: (err?: any, result?: any) => void) {
     const wrapped = tasks.map(fn => {
         return (args: any, cb: Function) => {
-            if (typeof args === 'function')
-                cb = args;
+            if (typeof args === 'function' && !cb)
+                return bind(fn, args, null);
 
             bind(fn, cb, args);
         }

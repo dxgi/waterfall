ed4ad05ed1f76f8fb3bb493c6dcefbc7e0f542fc
iMaximus <61899298+iiiMaximus@users.noreply.github.com>
2022-06-02 10:38:29 +0800

    release

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import async from 'async';
+
+const bind = async (fn: Function, cb: Function, args: any) => {
+    let result: any = null;
+
+    try { result = await fn(args); }
+    catch (err) { return cb(err); }
+
+    cb(null, result);
+}
+
+export default function waterfall<T>(tasks: Array<Function>, callback: (err?: any, result?: T) => void) {
+    const wrapped = tasks.map(fn => {
+        return (args: any, cb: Function) => {
+            if (typeof args === 'function')
+                cb = args;
+
+            bind(fn, cb, args);
+        }
+    });
+
+    async.waterfall<T>(wrapped, callback);
+}
